9d3d8983884b4d06b8b0e64e529e173a6f4a9a9c
Palvi005 <pxvi.005@gmail.com>
2026-03-31 21:31:54 +0530

    server setups

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,47 @@
+const express = require("express");
 
+const app = express();
+
+app.use(express.json());
+
+// test route
+app.get("/", (req, res) => {
+  res.send("CampusCycle Backend Running 🚀");
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
+
+
+let items = [];
+
+app.post("/add-item", (req, res) => {
+  const item = req.body;
+
+  item.status = "available";   
+  item.requests = [];         
+
+  items.push(item);
+
+  res.send("Item added successfully");
+});
+
+
+//view items
+app.get("/items", (req, res) => {
+  res.json(items);
+});
+
+//multiple students can send request
+app.post("/request-item", (req, res) => {
+  const { itemIndex, user } = req.body;
+
+  if (!items[itemIndex]) {
+    return res.send("Item not found");
+  }
+
+  items[itemIndex].requests.push(user);
+
+  res.send("Request sent");
+});
